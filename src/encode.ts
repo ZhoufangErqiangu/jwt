9d8f79151a3c8b8a21d80ee91ec713f1ca3031ea
alex <liuhlightning@qq.com>
2024-12-09 09:32:16 +0800

    fix type

--- src/encode.ts
+++ src/encode.ts
@@ -3,6 +3,6 @@
  * @param data The data to encode
  * @returns The base64 encoded data
  */
-export function encode(data: unknown) {
+export function encode(data: unknown): string {
   return Buffer.from(JSON.stringify(data)).toString("base64url");
 }
