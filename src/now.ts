dd158f7a308c8d53181874a629ae4eb8177183fe
alex <liuhlightning@qq.com>
2024-12-09 09:32:10 +0800

    doc

--- src/now.ts
+++ src/now.ts
@@ -1,3 +1,7 @@
-export function now() {
-  return Math.floor(Date.now() / 1000);
+/**
+ * Returns the current Unix timestamp in seconds.
+ * @returns The current Unix timestamp in seconds.
+ */
+export function now(): number {
+  return Math.floor(Date.now() / 1000.0);
 }
