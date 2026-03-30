34f8534466578a41d301dee7dcd312c656fc73c9
alex <liuhlightning@qq.com>
2026-03-30 18:40:10 +0800

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
 import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig({
   extends: [eslint.configs.recommended, tseslint.configs.recommended],
+  files: ["**/*.{ts}"],
   languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    globals: globals.node,
     parserOptions: {
       parser: tseslint.parser,
     },
@@ -12,10 +17,9 @@ export default defineConfig({
   // 0 off 1 warn 2 error
   rules: {
     "@typescript-eslint/ban-ts-comment": 0,
-    "@typescript-eslint/no-unused-vars": 0,
     quotes: [2, "double"],
     semi: [2, "always"],
     "no-console": 0,
   },
-  ignores: ["node_modules", "dist"],
+  ignores: ["node_modules/*", "dist/*", "build"],
 });
