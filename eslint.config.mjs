d7db7a2bbab1b9e603ef0f14650e2dc9ad10d980
alex <liuhlightning@qq.com>
2026-03-30 16:37:43 +0800

    fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,21 @@
 import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  {
-    // 0 off 1 warn 2 error
-    rules: {
-      "@typescript-eslint/ban-ts-comment": 0,
-      "@typescript-eslint/no-unused-vars": 0,
-      quotes: [2, "double"],
-      semi: [2, "always"],
-      "no-console": 0,
+export default defineConfig({
+  extends: [eslint.configs.recommended, tseslint.configs.recommended],
+  languageOptions: {
+    parserOptions: {
+      parser: tseslint.parser,
     },
-    ignores: ["node_modules", "dist"],
   },
-);
+  // 0 off 1 warn 2 error
+  rules: {
+    "@typescript-eslint/ban-ts-comment": 0,
+    "@typescript-eslint/no-unused-vars": 0,
+    quotes: [2, "double"],
+    semi: [2, "always"],
+    "no-console": 0,
+  },
+  ignores: ["node_modules", "dist"],
+});
